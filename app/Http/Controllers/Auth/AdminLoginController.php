84533e93c248f5c419b11415ff81183ca63cae75
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-02-25 11:59:17 -0700

    Part 2: Create login form for Admin

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('guest:admin');
+    }
+
+    public function showLoginForm()
+    {
+      return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+      // Validate the form data
+      $this->validate($request, [
+        'email'   => 'required|email',
+        'password' => 'required|min:6'
+      ]);
+
+      // Attempt to log the user in
+      if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+        // if successful, then redirect to their intended location
+        return redirect()->intended(route('admin.dashboard'));
+      }
+
+      // if unsuccessful, then redirect back to the login with the form data
+      return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/admin', 'AdminController@index');
+
+  Route::prefix('admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+  });
