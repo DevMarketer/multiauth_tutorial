2f1912813e5fb4a83c57e4d9936ad0514a4def3b
Petrica Nanca <petrica_nanca@yahoo.com>
2017-09-02 08:55:18 +0300

    Redirect to admin login page after logout

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -39,6 +39,6 @@ class AdminLoginController extends Controller
     public function logout()
     {
         Auth::guard('admin')->logout();
-        return redirect('/');
+        return redirect('/admin');
     }
 }
